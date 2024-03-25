028ee29f663595b2931c33bad63b803a85793567
acrease <alex@crease.sh>
2024-03-25 20:48:33 +0000

    more kitty config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -44,6 +44,11 @@
     font.name = "Fira Code";
     shellIntegration.enableZshIntegration = true;
     theme = "Aquarium Dark";
+    settings = {
+      font_features = "FiraCode-Retina +zero +onum";
+      cursor_shape = "beam";
+      window_margin_width = 10;
+    }
   };
 
   programs.starship = {
